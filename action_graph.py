c72ba5abc92a4d61196a347dc36f741c5fd846b1
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-07-30 13:04:06 +0800

    cuda

--- action_graph.py
+++ action_graph.py
@@ -58,8 +58,8 @@ class action_graph(nn.Module):
         edge_state_list = []
         #todo: can this be parallelized?
         for i in range(batch_size):
-            torch.nn.init.xavier_uniform_(vert_state)
-            torch.nn.init.xavier_uniform_(edge_state)
+            torch.nn.init.xavier_uniform(vert_state)
+            torch.nn.init.xavier_uniform(edge_state)
             vert_state = self.vert_gru(vert_input[i], vert_state)
             edge_state = self.edge_gru(edge_input[i], edge_state)
 
